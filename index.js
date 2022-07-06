86f9b6ed38da6cc53c25c82c1ceb12979933e1a9
Saraccostein <marco.vicp@tutanota.com>
2022-07-05 22:37:40 -0500

    El corazón aparece dando click

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ document.write('<span style="color:#786de7"> Estás en: ' + ruta + '<span>');
 
 var d = document.getElementById('dibujo');
 var lienzo = d.getContext('2d');
-console.log(xi);
 
-var texto = document.getElementById(texto_cariño)
-var botón_input = document.getElementById(botón)
+var texto = document.getElementById(texto_cariño);
+var botón_input = document.getElementById('botón');
 botón_input.addEventListener("click", dibujarPorClick);
 
 var línea_der = 3;
@@ -32,43 +31,41 @@ function dibujarLínea(color, x_inicial, y_inicial, anti_x_izq, anti_y_izq, anti
 
 function dibujarPorClick()
 {
-    alert('No me toques >.<');
-}
+    for(l = 0; l < línea_der; l++ )
+    {
+        color_rgb = '#786de7';
+        xi = 150;
+        yi = 100 + (l * 30);
 
-for(l = 0; l < línea_der; l++ )
-{
-    color_rgb = '#786de7';
-    xi = 150;
-    yi = 100 + (l * 30);
+        axz = 195 - (l * 15);
+        ayz = 35 + (l * 22);
 
-    axz = 195 - (l * 15);
-    ayz = 35 + (l * 22);
+        axd = 335 - (l * 35);
+        ayd = 120 + (l * 6);
 
-    axd = 335 - (l * 35);
-    ayd = 120 + (l * 6);
+        xf = 150;
+        yf = 250 - (l * 20);
 
-    xf = 150;
-    yf = 250 - (l * 20);
+        dibujarLínea(color_rgb, 150, yi, axz, ayz, axd, ayd, 150, yf);
+        console.log('línea derecha no.' + l);
+    }
 
-    dibujarLínea(color_rgb, 150, yi, axz, ayz, axd, ayd, 150, yf);
-    console.log('línea derecha no.' + l);
-}
+    for(l2 = 0; l2 < línea_izq; l2++ )
+    {
+        color_rgb = 'rgb(166, 99, 255)';
+        xi = 150;
+        yi = 100 + (l2 * 30);
 
-for(l2 = 0; l2 < línea_izq; l2++ )
-{
-    color_rgb = 'rgb(166, 99, 255)';
-    xi = 150;
-    yi = 100 + (l2 * 30);
+        axz = 105 + (l2 * 15);
+        ayz = 35 + (l2 * 22 + l2);
 
-    axz = 105 + (l2 * 15);
-    ayz = 35 + (l2 * 22 + l2);
+        axd = -35 + (l2 * 35);
+        ayd = 120 + (l2 * 6);
 
-    axd = -35 + (l2 * 35);
-    ayd = 120 + (l2 * 6);
+        xf = 150;
+        yf = 250 - (l2 * 20);
 
-    xf = 150;
-    yf = 250 - (l2 * 20);
-
-    dibujarLínea(color_rgb, 150, yi, axz, ayz, axd, ayd, 150, yf);
-    console.log('línea izquierda no.'+ l2);
+        dibujarLínea(color_rgb, 150, yi, axz, ayz, axd, ayd, 150, yf);
+        console.log('línea izquierda no.'+ l2);
+    }
 }
